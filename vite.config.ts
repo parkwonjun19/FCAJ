0f6525a14b680579caa841cb4b0fe4d350b007c4
WJP <park.wonjun19@gmail.com>
2025-05-09 20:29:43 +0900

    vite.config.js 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    open: true, // 이 부분이 브라우저 자동 오픈을 담당
+  },
   plugins: [react()],
 })
